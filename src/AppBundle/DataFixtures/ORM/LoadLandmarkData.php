3ffab102f5f15f0c747de3ac5ec5316b4dab7bed
Joe Harrison <whiplashomega@gmail.com>
2015-08-16 15:37:09 -0500

    landmarks

--- src/AppBundle/DataFixtures/ORM/LoadLandmarkData.php
+++ src/AppBundle/DataFixtures/ORM/LoadLandmarkData.php
@@ -24,18 +24,55 @@ HTML;
       $nextlandmark->setHtml($html);
       $manager->persist($nextlandmark);
       $nextlandmark = new Landmark();
-      $nextlandmark->setName("peloria");
+      $nextlandmark->setName("abyssalgateofdera");
       $html = <<<HTML
-  <h4>Peloria</h4>
+  <h4>Abyssal Gate of Dera</h4>
   <p>
-    The city of peloria was a rowdy bustling trading hub before the Nerim Cataclysm.  It was, at the time, one of the wealthiest and most cosmopolitan
-    cities of Malinval, even elves could walk the streets with a reasonable expectation of going unmolested.
+    High in the mountains above the ruined city of Dera, the abyssal gate that brought ruin to an empire is buried,
+    now closed by Bahamut and guarded constantly by ancient silver dragons, the gate's location is marked by a constant
+    smell of brimstone, and a complete lack of growing things.
   </p>
+HTML;
+      $nextlandmark->setHtml($html);
+      $manager->persist($nextlandmark);
+      $nextlandmark = new Landmark();
+      $nextlandmark->setName("abyssalgateofmarinda");
+      $html = <<<HTML
+  <h4>Abyssal Gate of Marinda</h4>
   <p>
-    After the cataclysm, armies of undead poured out of the subcontinent of Nerim, across what was then a relatively narrow channel between it and
-    Elathia. The unwalled and poorly defended city, far from the war fronts the orcs were used to fighting on, was quickly overrun.  Those who did not
-    escape were killed, and while the undead were driven back and eventually destroyed, the Champions of Cora who led the charge felt no pity for the
-    Mat'raktha worshipping orcs, and bypassed the city on their way to Nerim.  The city remains a nest of the undead to this day.
+    Marinda was, until recently, a bustling trading hub of the Morrind, until the appearance of an abyssal gate.  The
+    city is now an extension of the abyss into the mortal realm, a literal hell on earth.  The gate stands open in what
+    was once the center of the city, and demons move freely between this realm and their own.  It is a war that has
+    consumed the Morrind for nearly a year now, as they desperately try to contain the demon invasion.  It is also the
+    first war in a thousand years that the trollkin have fought on their own behalf, as they have marshalled all of
+    their available armies to stem the tide, and keep the demons from reaching their homeland of Degak'Ta.
+  </p>
+HTML;
+      $nextlandmark->setHtml($html);
+      $manager->persist($nextlandmark);
+      $nextlandmark = new Landmark();
+      $nextlandmark->setName("dalokra");
+      $html = <<<HTML
+  <h4>Dal Okra</h4>
+  <p>
+    Literally in Orcish "Dal Okra" means Home of the Orcs. It is an ancient name for the place of legend where the
+    orc race was first created. Whether that was actually the huge black mountain called Dal Okra today is a question
+    for debate, as that history was lost in the God's War. Dal Okra today is a huge empty tomb, once holding the Prince of
+    Fiends, a believed dead Sorceron, who once commanded at least part of the orc legions in the God's War before he
+    was struck down by Michael, the Archangel of Alohim.  Now he is awake again, and has taken nearly complete control
+    of the orcish tribes.
+  </p>
+HTML;
+      $nextlandmark->setHtml($html);
+      $manager->persist($nextlandmark);
+      $nextlandmark = new Landmark();
+      $nextlandmark->setName("Dera");
+      $html = <<<HTML
+  <h4>Dera</h4>
+  <p>
+    Once the capital of the Deran empire, now an ancient ruin destroyed by demons.  Yet it is not uninhabited.
+    Underground, in the deep sewers, live the Fey'ri, a bastard race of elves and demons.  The spawn of the inhabitants
+    of the first towns to be destroyed by the demons.  
   </p>
 HTML;
       $nextlandmark->setHtml($html);
@@ -43,16 +80,14 @@ HTML;
       $nextlandmark = new Landmark();
       $nextlandmark->setName("peloria");
       $html = <<<HTML
-  <h4>Peloria</h4>
+  <h4>Dal Okra</h4>
   <p>
-    The city of peloria was a rowdy bustling trading hub before the Nerim Cataclysm.  It was, at the time, one of the wealthiest and most cosmopolitan
-    cities of Malinval, even elves could walk the streets with a reasonable expectation of going unmolested.
-  </p>
-  <p>
-    After the cataclysm, armies of undead poured out of the subcontinent of Nerim, across what was then a relatively narrow channel between it and
-    Elathia. The unwalled and poorly defended city, far from the war fronts the orcs were used to fighting on, was quickly overrun.  Those who did not
-    escape were killed, and while the undead were driven back and eventually destroyed, the Champions of Cora who led the charge felt no pity for the
-    Mat'raktha worshipping orcs, and bypassed the city on their way to Nerim.  The city remains a nest of the undead to this day.
+    Literally in Orcish "Dal Okra" means Home of the Orcs. It is an ancient name for the place of legend where the
+    orc race was first created. Whether that was actually the huge black mountain called Dal Okra today is a question
+    for debate, as that history was lost in the God's War. Dal Okra today is a huge empty tomb, once holding the Prince of
+    Fiends, a believed dead Sorceron, who once commanded at least part of the orc legions in the God's War before he
+    was struck down by Michael, the Archangel of Alohim.  Now he is awake again, and has taken nearly complete control
+    of the orcish tribes.
   </p>
 HTML;
       $nextlandmark->setHtml($html);
@@ -60,16 +95,29 @@ HTML;
       $nextlandmark = new Landmark();
       $nextlandmark->setName("peloria");
       $html = <<<HTML
-  <h4>Peloria</h4>
+  <h4>Dal Okra</h4>
   <p>
-    The city of peloria was a rowdy bustling trading hub before the Nerim Cataclysm.  It was, at the time, one of the wealthiest and most cosmopolitan
-    cities of Malinval, even elves could walk the streets with a reasonable expectation of going unmolested.
+    Literally in Orcish "Dal Okra" means Home of the Orcs. It is an ancient name for the place of legend where the
+    orc race was first created. Whether that was actually the huge black mountain called Dal Okra today is a question
+    for debate, as that history was lost in the God's War. Dal Okra today is a huge empty tomb, once holding the Prince of
+    Fiends, a believed dead Sorceron, who once commanded at least part of the orc legions in the God's War before he
+    was struck down by Michael, the Archangel of Alohim.  Now he is awake again, and has taken nearly complete control
+    of the orcish tribes.
   </p>
+HTML;
+      $nextlandmark->setHtml($html);
+      $manager->persist($nextlandmark);
+      $nextlandmark = new Landmark();
+      $nextlandmark->setName("peloria");
+      $html = <<<HTML
+  <h4>Dal Okra</h4>
   <p>
-    After the cataclysm, armies of undead poured out of the subcontinent of Nerim, across what was then a relatively narrow channel between it and
-    Elathia. The unwalled and poorly defended city, far from the war fronts the orcs were used to fighting on, was quickly overrun.  Those who did not
-    escape were killed, and while the undead were driven back and eventually destroyed, the Champions of Cora who led the charge felt no pity for the
-    Mat'raktha worshipping orcs, and bypassed the city on their way to Nerim.  The city remains a nest of the undead to this day.
+    Literally in Orcish "Dal Okra" means Home of the Orcs. It is an ancient name for the place of legend where the
+    orc race was first created. Whether that was actually the huge black mountain called Dal Okra today is a question
+    for debate, as that history was lost in the God's War. Dal Okra today is a huge empty tomb, once holding the Prince of
+    Fiends, a believed dead Sorceron, who once commanded at least part of the orc legions in the God's War before he
+    was struck down by Michael, the Archangel of Alohim.  Now he is awake again, and has taken nearly complete control
+    of the orcish tribes.
   </p>
 HTML;
       $nextlandmark->setHtml($html);
@@ -92,10 +140,6 @@ HTML;
       $nextlandmark->setHtml($html);
       $manager->persist($nextlandmark);
       /*
-Abyssal Gate of Dera
-Abyssal Gate of Marinda
-Cur'Estan
-Dal Okra
 Dera
 Fang
 Fortress of Light
@@ -103,6 +147,8 @@ Gaia's Grove
 God Spire
 Great Ramp
 Iron Triangle
+Nerim
+Peloria
 Sorcerer's Prison
        */
       $manager->flush();
