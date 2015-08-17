d17cb41d64a36d3745b4416c37ed1f10f7f1d4e9
Joe.Harrison <Joe.Harrison@wisconsin.gov>
2015-08-17 11:20:12 -0500

    landmark descriptions done

--- src/AppBundle/DataFixtures/ORM/LoadLandmarkData.php
+++ src/AppBundle/DataFixtures/ORM/LoadLandmarkData.php
@@ -78,46 +78,114 @@ HTML;
       $nextlandmark->setHtml($html);
       $manager->persist($nextlandmark);
       $nextlandmark = new Landmark();
-      $nextlandmark->setName("peloria");
+      $nextlandmark->setName("fang");
       $html = <<<HTML
-  <h4>Dal Okra</h4>
+  <h4>Fang</h4>
   <p>
-    Literally in Orcish "Dal Okra" means Home of the Orcs. It is an ancient name for the place of legend where the
-    orc race was first created. Whether that was actually the huge black mountain called Dal Okra today is a question
-    for debate, as that history was lost in the God's War. Dal Okra today is a huge empty tomb, once holding the Prince of
-    Fiends, a believed dead Sorceron, who once commanded at least part of the orc legions in the God's War before he
-    was struck down by Michael, the Archangel of Alohim.  Now he is awake again, and has taken nearly complete control
-    of the orcish tribes.
+    The white mountain, Fang, is the center of Sand Orc society. The mountain is home to an extensive system of natural caves, which they have greatly
+    enlarged, and several cool springs that supply an abundance of water. As such it serves as trading post and wintering location for the Sand Orcs. It
+    is also the location their chiefs meet in order to discuss any matters that involve all of the tribes. It is a place of peace, and no sand orc is
+    allowed to raise a hand against another while at or near Fang.
   </p>
 HTML;
       $nextlandmark->setHtml($html);
       $manager->persist($nextlandmark);
       $nextlandmark = new Landmark();
-      $nextlandmark->setName("peloria");
+      $nextlandmark->setName("fortressoflight");
       $html = <<<HTML
-  <h4>Dal Okra</h4>
+  <h4>Fortress of Light</h4>
   <p>
-    Literally in Orcish "Dal Okra" means Home of the Orcs. It is an ancient name for the place of legend where the
-    orc race was first created. Whether that was actually the huge black mountain called Dal Okra today is a question
-    for debate, as that history was lost in the God's War. Dal Okra today is a huge empty tomb, once holding the Prince of
-    Fiends, a believed dead Sorceron, who once commanded at least part of the orc legions in the God's War before he
-    was struck down by Michael, the Archangel of Alohim.  Now he is awake again, and has taken nearly complete control
-    of the orcish tribes.
+    The Fortress of Light is a massive joint venture defensive fortress and staging facility manned by dwarves, humans, and elves from Gerasalim,
+    Dormania, Kandor, Curinor, Demal Thor, and Eldoran. It is the central supply dump and gathering point for campaigns and troop movements in the
+    defense against attacking orcs, trolls, gnolls, and various goblinoids coming across arid plain known as the Blighted Gap.
+  </p>
+  <p>
+    The fortress itself is built surrounding a small springfed lake, and covers nearly 3 square miles. It's outer wall extend up 150 feet of sheer stone
+    braced with adamantium, without joint or crack, and they are nearly 15 foot thick. Several large inner keeps, built similarly to the outer wall,
+    extend up some 200-250 feet. Large counterspelling crystals instantly counter any spell cast at or near the fortress. A dry moat, filled with thick
+    steel spikes, extends out 25 feet from the edge of the wall.
   </p>
 HTML;
       $nextlandmark->setHtml($html);
       $manager->persist($nextlandmark);
       $nextlandmark = new Landmark();
-      $nextlandmark->setName("peloria");
+      $nextlandmark->setName("gaiasgrove");
       $html = <<<HTML
-  <h4>Dal Okra</h4>
+  <h4>Gaia's Grove</h4>
   <p>
-    Literally in Orcish "Dal Okra" means Home of the Orcs. It is an ancient name for the place of legend where the
-    orc race was first created. Whether that was actually the huge black mountain called Dal Okra today is a question
-    for debate, as that history was lost in the God's War. Dal Okra today is a huge empty tomb, once holding the Prince of
-    Fiends, a believed dead Sorceron, who once commanded at least part of the orc legions in the God's War before he
-    was struck down by Michael, the Archangel of Alohim.  Now he is awake again, and has taken nearly complete control
-    of the orcish tribes.
+    The Dreaming Goddess, Gaia, Who Dreams No More.  Such is the line the druids have told in recent months, encouraging pilgrims to come to the
+    wild island of Gnarra to see the hidden grove where Gaia, until recently, slept.  About 20 miles in from the south coast of Gnarra the grove is
+    guarded by the Faelin, who speak little about the events that led to the Goddess awakening, only saying she was 'saved' by a group of adventurers
+    and a mighty druid.  No one will say what she was saved from.
+  </p>
+  <p>
+    The grove itself has only 1 small entrance, with no other path through the tangle of branches and trees that form an impenetrable dome around the
+    living altar.
+  </p>
+HTML;
+      $nextlandmark->setHtml($html);
+      $manager->persist($nextlandmark);
+      $nextlandmark = new Landmark();
+      $nextlandmark->setName("gaiasgrove");
+      $html = <<<HTML
+  <h4>Gaia's Grove</h4>
+  <p>
+    The Dreaming Goddess, Gaia, Who Dreams No More.  Such is the line the druids have told in recent months, encouraging pilgrims to come to the
+    wild island of Gnarra to see the hidden grove where Gaia, until recently, slept.  About 20 miles in from the south coast of Gnarra the grove is
+    guarded by the Faelin, who speak little about the events that led to the Goddess awakening, only saying she was 'saved' by a group of adventurers
+    and a mighty druid.  No one will say what she was saved from.
+  </p>
+  <p>
+    The grove itself has only 1 small entrance, with no other path through the tangle of branches and trees that form an impenetrable dome around the
+    living altar.
+  </p>
+HTML;
+      $nextlandmark->setHtml($html);
+      $manager->persist($nextlandmark);
+      $nextlandmark = new Landmark();
+      $nextlandmark->setName("godspire");
+      $html = <<<HTML
+  <h4>God Spire</h4>
+  <p>
+    The God Spire is a relic of the age before the God's War located in north central Eldoran. It is a 500 foot tall metallic spire, with inscriptions
+    covering it written in an unknown heiroglyphic language. It is believed to be a shrine once dedicated to some now dead God, but no one really knows
+    its purpose or what may be inside.
+  </p>
+HTML;
+      $nextlandmark->setHtml($html);
+      $manager->persist($nextlandmark);
+      $nextlandmark = new Landmark();
+      $nextlandmark->setName("greatramp");
+      $html = <<<HTML
+  <h4>Great Ramp</h4>
+  <p>
+    The Great Ramp was built by Dorman III to connect the wizards college at Malidal with the rest of the empire without having to travel hundreds of
+    miles by sea. It was a massive public works project that employed nearly 300,000 people during a period of relative peace for the Empire and
+    construction took 23 years. Shortly after its completion Dorman III died of a heart attack.
+  </p>
+HTML;
+      $nextlandmark->setHtml($html);
+      $manager->persist($nextlandmark);
+      $nextlandmark = new Landmark();
+      $nextlandmark->setName("irontraingle");
+      $html = <<<HTML
+  <h4>Iron Triangle</h4>
+  <p>
+    The Iron Triangle is three fortresses built equidistant from each other in northeast Nerim, each about half a mile from the other two. In their day
+    the three fortresses were the greatest defensive structure in the world, although they never endured much test beyond disorganized rebel groups.
+    Today they are (mostly) uninhabited ruins, but they are still imposing and difficult to capture should anyone choose to defend them.
+  </p>
+HTML;
+      $nextlandmark->setHtml($html);
+      $manager->persist($nextlandmark);
+      $nextlandmark = new Landmark();
+      $nextlandmark->setName("nerim");
+      $html = <<<HTML
+  <h4>Nerim</h4>
+  <p>
+    Nerim was once the seat of the Neran Empire, a monotheistic continent spanning empire.  It was here that the great spell was cast that turned the
+    population of the continent into armies of the undead, millions strong.  The city still stands, largely undamaged, preserved by the dry air, and
+    the inability of growing things to survive in the lingering negative energy presence.
   </p>
 HTML;
       $nextlandmark->setHtml($html);
@@ -139,18 +207,19 @@ HTML;
 HTML;
       $nextlandmark->setHtml($html);
       $manager->persist($nextlandmark);
-      /*
-Dera
-Fang
-Fortress of Light
-Gaia's Grove
-God Spire
-Great Ramp
-Iron Triangle
-Nerim
-Peloria
-Sorcerer's Prison
-       */
+      $nextlandmark = new Landmark();
+      $nextlandmark->setName("sorcerersprison");
+      $html = <<<HTML
+  <h4>Sorcerer's Prison</h4>
+  <p>
+    The Sorcerer's Prison is believed to be the single greatest work of magic since the God's War. Created by the wizards of Zelfir to contain their once
+    master, a powerful Sorceron enchanter of unknown name, it is a huge magical barrier which it is possible to enter from the outside, but impossible to
+    get out of from within. The barrier forms a massive sphere, with the center at ground level such that it is impossible to fly or dig oneself out. The
+    surface of the barrier is silvery and impossible to see through. The barrier has stood for thousands of years without anyone ever escaping.
+  </p>
+HTML;
+      $nextlandmark->setHtml($html);
+      $manager->persist($nextlandmark);
       $manager->flush();
       }
   }
