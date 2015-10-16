d2299f57aa059066cb405ef13beeb1ebfe9d0388
Joe.Harrison <Joe.Harrison@wisconsin.gov>
2015-10-16 16:25:57 -0500

    basic layout and structure in single page app

---
+++ src/AppBundle/js/tools.js
@@ -0,0 +1,108 @@
+(function($, ng) {
+    var dmTools = ng.module('dmTools', []);
+  
+    dmTools.controller('appMenu', function($scope, $http) {
+    
+      $scope.loadCreatureCreator = function() {
+        $(".app").hide();
+        $("#creatureCreator").toggle('slide', {direction: 'right', duration: 500});
+        $(".AppMenuButton").removeClass("btn-primary");
+        $("#loadCreatureCreator").addClass("btn-primary");
+      }
+      
+      $scope.loadNpcBuilder = function() {
+        $(".app").hide();
+        $("#npcBuilder").toggle('slide', {direction: 'right', duration: 500});
+        $(".AppMenuButton").removeClass("btn-primary");
+        $("#loadNpcBuilder").addClass("btn-primary");
+      }
+      
+      $scope.loadEncounterBuilder = function() {
+        $(".app").hide();
+        $("#encounterBuilder").toggle('slide', {direction: 'right', duration: 500});
+        $(".AppMenuButton").removeClass("btn-primary");
+        $("#loadEncounterBuilder").addClass("btn-primary");
+      }
+      
+      $scope.loadReferenceManual = function() {
+        $(".app").hide();
+        $("#referenceManual").toggle('slide', {direction: 'right', duration: 500});
+        $(".AppMenuButton").removeClass("btn-primary");
+        $("#loadReferenceManual").addClass("btn-primary");
+      }
+      
+      $scope.loadInitTracker = function() {
+        $(".app").hide();
+        $("#initTracker").toggle('slide', {direction: 'right', duration: 500});
+        $(".AppMenuButton").removeClass("btn-primary");
+        $("#loadInitTracker").addClass("btn-primary");
+      }
+      
+      $scope.loadDmJournal = function() {
+        $(".app").hide();
+        $("#dmJournal").toggle('slide', {direction: 'right', duration: 500});
+        $(".AppMenuButton").removeClass("btn-primary");
+        $("#loadDmJournal").addClass("btn-primary");
+      }
+      
+      $scope.loadDiceRoller = function() {
+        $(".app").hide();
+        $("#diceRoller").toggle('slide', {direction: 'right', duration: 500});
+        $(".AppMenuButton").removeClass("btn-primary");
+        $("#loadDiceRoller").addClass("btn-primary");
+      }
+      
+      $scope.loadDungeonMap = function() {
+        $(".app").hide();
+        $("#dungeonMap").toggle('slide', {direction: 'right', duration: 500});
+        $(".AppMenuButton").removeClass("btn-primary");
+        $("#loadDungeonMap").addClass("btn-primary");
+      }
+      
+      $scope.init = function() {
+        //Assign callbacks
+        $("#sidebar").addClass('hidden');
+        $("#main").removeClass('col-md-10').addClass('col-md-12');
+        $("#loadCreatureCreator").click($scope.loadCreatureCreator);
+        $("#loadNpcBuilder").click($scope.loadNpcBuilder);
+        $("#loadEncounterBuilder").click($scope.loadEncounterBuilder);
+        $("#loadReferenceManual").click($scope.loadReferenceManual);
+        $("#loadInitTracker").click($scope.loadInitTracker);
+        $("#loadDmJournal").click($scope.loadDmJournal);
+        $("#loadDiceRoller").click($scope.loadDiceRoller);
+        $("#loadDungeonMap").click($scope.loadDungeonMap);
+      }
+    });
+    
+    dmTools.controller('creatureCreator', function($scope, $http) {
+      
+    });
+    
+    dmTools.controller('npcBuilder', function($scope, $http) {
+      
+    });
+    
+    dmTools.controller('encounterBuilder', function($scope, $http) {
+      
+    });
+    
+    dmTools.controller('referenceManual', function($scope, $http) {
+      
+    });
+    
+    dmTools.controller('initTracker', function($scope, $http) {
+      
+    });
+    
+    dmTools.controller('dmJournal', function($scope, $http) {
+      
+    });
+    
+    dmTools.controller('diceRoller', function($scope, $http) {
+      
+    });
+    
+    dmTools.controller('dungeonMap', function($scope, $http) {
+      
+    });
+})(jQuery, angular);
